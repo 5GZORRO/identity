ba02663bc95e67f2d55bc254e7f0fd64c4b8f79f
MiguelASilva98 <36764282+MiguelASilva98@users.noreply.github.com>
2021-03-10 10:43:01 +0000

    added docker for demo handler

--- src/handler_example/handler.py
+++ src/handler_example/handler.py
@@ -1,7 +1,7 @@
 import requests, json, os
 from flask import Flask, request, jsonify
-from dotenv import load_dotenv
-load_dotenv()
+#from dotenv import load_dotenv
+#load_dotenv()
 
 app = Flask(__name__)
 
